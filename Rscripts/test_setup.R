371ace9d2f2e2dc8e4a8d74035bb2b1a3621f860
Malcolm Morgan <mem48@users.noreply.github.com>
2018-08-06 17:15:49 +0100

    test elevation

--- Rscripts/test_setup.R
+++ Rscripts/test_setup.R
@@ -1,6 +1,9 @@
 dir = "F:/otp-tutorial-master/otp-tutorial-master/demo"
 memory = 60
-#otp_build_graph(dir,memory = memory)
+library(opentripplanner)
+library(tmap)
+tmap_mode("view")
+otp_build_graph(dir,memory = memory)
 otp_setup(dir,
           memory = 2,
           router = "current",
@@ -8,11 +11,15 @@ otp_setup(dir,
           securePort = 8802)
 
 otpcon <- otp_connect("localhost","current",8801)
-fromPlace = c("53.49703","-2.25306")
-toPlace = c("53.47252","-2.24413")
+fromPlace = c("53.43306", "-2.23881")
+toPlace = c("53.52970", "-2.26456")
 
 route = otp_plan(otpcon, fromPlace = fromPlace, toPlace = toPlace )
-qtm(route)
 
+qtm(sf::st_zm(route))
+# plot the elevation
+coords = st_coordinates(route)
+plot(coords[,3] / 0.3048)
 
 otp_stop()
+
